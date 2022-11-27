d632afa9023970770a7d67680aa83c7ef31f0f3a
Andri Kasian <97379652+AndryPanda@users.noreply.github.com>
2022-11-27 18:22:39 +0200

    two
    two

---
+++ two/php/index.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST['email'];
+$message = $_POST['message'];
+echo 'Your name: ' . $name.'<br>';
+echo 'Your email: ' . $email.'<br>';
+echo $message . '<br>';
+?>
